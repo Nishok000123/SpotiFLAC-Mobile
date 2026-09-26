fc4157241728eee727b19a28819d7c265b286669
zarzet <42882290+zarzet@users.noreply.github.com>
2026-09-26 12:45:16 +0700

    chore(release): bump app version to 5.0.5

--- lib/constants/app_info.dart
+++ lib/constants/app_info.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/foundation.dart';
 
 class AppInfo {
-  static const String version = '5.0.1';
-  static const String buildNumber = '153';
+  static const String version = '5.0.5';
+  static const String buildNumber = '154';
   static const String fullVersion = '$version+$buildNumber';
 
   static const String gitCommit = String.fromEnvironment('GIT_COMMIT');
